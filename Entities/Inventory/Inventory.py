b8fa3e7e261edb3fd6e91fd1e5603325769f2190
Christopher Bell <Yui@Yui.local>
2016-10-03 21:40:44 -0500

    Fixed naming convention for Inventory.

---
+++ Entities/Inventory/Inventory.py
@@ -0,0 +1,18 @@
+class Inventory(object):
+
+	def __init__(self):
+		self.slots = {'Wood':0, 'Shelter':0, 'Boat':0}
+		self.slotsMaxSize = 10
+
+	def display(self):
+		for key, value in self.slots.items():
+			print(key,":", value)
+		print("\n")
+
+
+	def isSlotsFull(self):
+		if(sum(self.slots.values()) >= self.slotsMaxSize):
+			return True
+		else:
+			return False
+
