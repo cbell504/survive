2da0967ea51a0cc66f9db5a4ad6263122d868d17
Christopher Bell <chrisotpherbell1813@gmail.com>
2016-10-05 23:14:51 -0500

    You can now kill enemies.

--- Entities/Enemies/EnemyController.py
+++ Entities/Enemies/EnemyController.py
@@ -15,51 +15,5 @@ class EnemyController(Controller):
 	def getEnemyAttack(self):
 		return self.enemy.basicAttack()
 
-	def start(self, player):
-
-		# 
-		# Generate enemy here
-		# Create spawner class and spawner will talk to combat to say what the enemy is doing
-		# 
-
-		enemy = Hog
-		while True:
-			playerInput = -1
-			try:
-				self.huntingView.displayStart()
-
-				playerInput = int(input("Enter an action.\n"))
-				print("\n")
-
-				self.clearScreen()
-
-				if(playerInput == 0 ):
-					self.huntingView.displayEnd()
-					break
-
-				elif(playerInput == 10):
-					self.clearScreen()
-
-				elif(playerInput == 1):
-					if(self.hunting.chanceToFindAnimal()):
-						combatController = CombatController()
-						combatController.start(player)
-					else:
-						self.huntingView.displayNoAnimalFound()
-
-
-					
-
-				else:
-					print("This is not a valid action\n")
-
-			except ValueError:
-				print("Please enter a number.\n")
-			except:
-				print("Error occurred.\n")
-				raise
-
-		return player
-
 
 
