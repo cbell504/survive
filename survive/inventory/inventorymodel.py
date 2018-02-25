65c7646dcb96a8145947043e1c99bb893c62628e
Christopher Bell <christopherbell1813@gmail.com>
2018-02-25 11:24:22 -0600

    Changed member variables to private, added getters

--- survive/inventory/inventorymodel.py
+++ survive/inventory/inventorymodel.py
@@ -1,28 +1,39 @@
 class Inventory(object):
-
     def __init__(self):
-        self.slots = {'Wood': 0, 'Shelter': 0, 'Boat': 0}
-        self.slots_max_size = 10
+        self._slots_max_size = 10
+        self._slots = {
+            'Wood': 0,
+            'Shelter': 0,
+            'Boat': 0}
 
     def add_item(self, item, total_items):
-        self.slots.update({item: self.slots.get(item) + total_items})
+        self._slots.update({item: self._slots.get(item) + total_items})
 
     def display(self):
-        for key, value in self.slots.items():
+        for key, value in self._slots.items():
             print(key, ":", value)
         print("\n")
 
     def get_item(self, item):
         try:
-            return self.slots.get(item)
+            return self._slots.get(item)
         except:
             print("Item does not exist")
 
+    def get_slots(self):
+        return self._slots
+    
+    def get_slots_max_size(self):
+        self._slots_max_size
+    
+    def increase_max_size(self, max_size):
+        self._slots_max_size = max_size
+
     def is_slots_full(self):
-        if(sum(self.slots.values()) >= self.slots_max_size):
+        if(sum(self._slots.values()) >= self._slots_max_size):
             return True
         else:
             return False
 
     def remove_item(self, item, total_items):
-        self.slots.update({item: self.slots.get(item) - total_items})
+        self._slots.update({item: self._slots.get(item) - total_items})
